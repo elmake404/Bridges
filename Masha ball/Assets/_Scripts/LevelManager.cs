d5e8e5f3796936aa11bb1b93eda3ae699a44dfb4
DESKTOP-19MJ94B\Admin <kibirevvalik202@gmail.com>
2020-08-17 14:16:32 +0300

    [add] navMesh

---
+++ Masha ball/Assets/_Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    #region Static variables
+    public static bool IsStartGame;
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Masha ball/Assets/_Scripts/PlayerContorol.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerContorol : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent _agent;
+    [SerializeField]
+    private Transform _target;
+    void Start()
+    {
+        _agent.SetDestination(_target.position);
+    }
+
+    void Update()
+    {
+        
+    }
+}
