58ba649e16c976b1000d4f02ddff4606de8a2cf3
valentin202 <kibirevvalik202@gmail.com>
2020-08-18 17:07:18 +0300

    [update] player control

--- Masha ball/Assets/_Scripts/PlayerContorol.cs
+++ Masha ball/Assets/_Scripts/PlayerContorol.cs
@@ -9,13 +9,36 @@ public class PlayerContorol : MonoBehaviour
     private NavMeshAgent _agent;
     [SerializeField]
     private Transform _target;
+    private Vector3 _agentTarget;
+    [SerializeField]
+    List<Vector3> _way = new List<Vector3>(); 
+
     void Start()
     {
+        _agent.updatePosition = false;
         _agent.SetDestination(_target.position);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RecordsWay();
+        }
+        //transform.position = Vector3.MoveTowards(transform.position, _agent.steeringTarget, 0.1f);
+    }
+
+    private void RecordsWay()
+    {
+        while (_way.Count<12)
+        {
+            _agent.nextPosition = _agent.steeringTarget;
+            if (_agent.steeringTarget != _agentTarget)
+            {
+                _agentTarget = _agent.steeringTarget;
+                _way.Add(_agentTarget);
+            }
+
+        }
     }
 }
