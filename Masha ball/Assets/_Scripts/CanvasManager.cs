2ea0b4f715d75d52d2c6f302e683fedbfff2d609
Valentin <kibirevvalik202@gmail.com>
2020-08-20 17:56:23 +0300

    [update] beamControl

--- Masha ball/Assets/_Scripts/CanvasManager.cs
+++ Masha ball/Assets/_Scripts/CanvasManager.cs
@@ -6,9 +6,10 @@ public class CanvasManager : MonoBehaviour
 {
     private Camera _cam;
     private Transform _objMoving;
-    private Vector3 _oldPosObj,_startPosMouse;
+    private Vector3 _oldPosObj, _startPosMouse;
 
-    private bool _isCast;
+    private bool _isCastDrag, _isCastTwist;
+    private float _rotationY;
 
     void Start()
     {
@@ -22,32 +23,59 @@ public class CanvasManager : MonoBehaviour
         {
             RaycastHit hit;
             Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit)&&(hit.collider.tag=="Beam"))
+            if (Physics.Raycast(ray, out hit))
             {
-                _isCast = true;
-                _objMoving = hit.collider.transform.parent;
-                _oldPosObj = _objMoving.position;
-                _startPosMouse = (_cam.transform.position - ((ray.direction) *
-                    (((22.21f) - 0.05f) / ray.direction.y)));
+                if ((hit.collider.tag == "Beam"))
+                {
+                    _isCastDrag = true;
+                    _objMoving = hit.collider.transform.parent;
+                    _oldPosObj = _objMoving.position;
+                    _startPosMouse = (_cam.transform.position - ((ray.direction) *
+                        (((22.21f) - 0.05f) / ray.direction.y)));
+                }
+                else if (hit.collider.tag == "Piston")
+                {
+                    _isCastTwist = true;
+
+                    _objMoving = hit.collider.transform.parent;
+                    _startPosMouse = (_cam.transform.position - ((ray.direction) *
+                        (((22.21f) - 0.05f) / ray.direction.y)));
+                    _rotationY = Quaternion.LookRotation(_startPosMouse - _objMoving.position).eulerAngles.y;
+                }
             }
         }
         else if (Input.GetMouseButton(0))
         {
-            if (_isCast)
+            if (_isCastDrag)
             {
                 Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
 
                 _objMoving.position = _oldPosObj + ((_cam.transform.position - ((ray.direction) *
-                    (((22.21f) - 0.05f) / ray.direction.y)))- _startPosMouse);
+                    (((22.21f) - 0.05f) / ray.direction.y))) - _startPosMouse);
 
+            }
+            if (_isCastTwist)
+            {
+                Ray ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+                _startPosMouse = (_cam.transform.position - ((ray.direction) *
+                    (((22.21f) - 0.05f) / ray.direction.y)));
+
+                _objMoving.rotation = Quaternion.LookRotation(_startPosMouse - _objMoving.position);
             }
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            if (_isCast)
+            if (_isCastDrag)
             {
-                _isCast = false;
+                _isCastDrag = false;
                 _objMoving = null;
+            }
+            if (_isCastTwist)
+            {
+                _isCastTwist = false;
+                _objMoving = null;
+
             }
         }
     }
